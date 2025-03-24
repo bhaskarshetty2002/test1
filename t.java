8c1e6f291ac2faa4dc7a4d3f7efcdf082135a001
Bhaskar <110278343+bhaskarshetty2002@users.noreply.github.com>
2025-03-24 15:32:58 +0530

    Update t.java

--- t.java
+++ t.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int a = 5; 
-        int b = 3;
+        int b = 39;
         int sum = a + b; 
         System.out.println(sum);
     }
