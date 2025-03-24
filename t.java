3ab8668dfa151d783bcba3cae4603390d81ed72d
Bhaskar <110278343+bhaskarshetty2002@users.noreply.github.com>
2025-03-24 15:12:30 +0530

    Update t.java

--- t.java
+++ t.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int a = 5; 
-        int b = 16;
+        int b = 36;
         int sum = a + b; 
         System.out.println(sum);
     }
